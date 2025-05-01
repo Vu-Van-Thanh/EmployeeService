e3d619b005180af29818ba821d0e3aee2bcfede5
VuVanThanh <vut4262@gmail.com>
2025-05-01 09:20:15 +0700

    test

--- EmployeeService.Infrastructure/Kafka/Handlers/EmployeeImportHandler.cs
+++ EmployeeService.Infrastructure/Kafka/Handlers/EmployeeImportHandler.cs
@@ -4,16 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using EmployeeService.Core.DTO;
+using EmployeeService.Core.Services;
 
 namespace EmployeeService.Infrastructure.Kafka.Handlers
 {
     public class EmployeeImportHandler : IKafkaHandler<KafkaRequest<StartImportEmployee>>
     {
         
-        public Task HandleAsync(KafkaRequest<StartImportEmployee> message)
+        private readonly IFileService _fileService;
+        private readonly IEmployeeService _employeeService;
+        public EmployeeImportHandler(IFileService fileService, IEmployeeService employeeService)
         {
-            var fileBytes = Convert.FromBase64String(message.Data.FileContent);
-            v
+            _fileService = fileService;
+            _employeeService = employeeService;
         }
+        public async Task HandleAsync(KafkaRequest<StartImportEmployee> message)
+        {
+            var fileContent = message.Filter.FileContent;
+            var base64Content = Convert.FromBase64String(fileContent);
+            var fileBytes = base64Content.ToArray();
+            var fileName = $"EmployeeImport_{Guid.NewGuid()}.xlsx";
+            IFormFile file = _fileService.CreateFormFile(fileBytes, fileName);
+            EmployeeImportDTO result = await _employeeService.ImportProfileFromExcelAsync(file);
+            KafkaResponse<EmployeeImportDTO> kafkaResponse = new KafkaResponse<EmployeeImportDTO>
+            {
+                RequestType = "ImportedEmployee",
+                CorrelationId = message.CorrelationId,
+                Timestamp = DateTime.UtcNow,
+                Filter = result
+            };
+            await _kafkaProducer.ProduceAsync("ImportedEmployee",null,"ImportedEmployee",kafkaResponse);
+
+        }
+
+
+        
     }
 }

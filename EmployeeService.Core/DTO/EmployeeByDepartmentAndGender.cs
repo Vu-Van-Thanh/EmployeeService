95df8bca1df68ac2acfed0d79fcdb83f424205cb
ThanhVu <vut4262@gmail.com>
2025-05-13 16:41:31 +0700

    te

---
+++ EmployeeService.Core/DTO/EmployeeByDepartmentAndGender.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeService.Core.DTO
+{
+    public class EmployeeByDepartmentAndGender
+    {
+        public string departmentName { get; set; }
+        public int male {  get; set; }
+        public int female { get; set; }
+
+    }
+}
